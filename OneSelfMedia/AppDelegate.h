e1e9060e548b2331bccaf5f27e96dfbc13775b15
EG365 <xujun@xujunmatoMacBook-Pro.local>
2012-11-28 10:14:47 +0800

    Initial Commit

---
+++ OneSelfMedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OneSelfMedia
+//
+//  Created by EG365 on 12-11-28.
+//  Copyright (c) 2012年 EG365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneSelfMedia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OneSelfMedia
+//
+//  Created by EG365 on 12-11-28.
+//  Copyright (c) 2012年 EG365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
